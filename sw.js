c84fda3df1cabfe3c58ac7b7ee9883d9d08b4f70
jhosbrr <vare22jhos@gmail.com>
2026-07-30 10:37:44 -0600

    Bricks 3.1.0

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='bricks-v3-0-official';
+const CACHE='bricks-v3-0-1-available';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
